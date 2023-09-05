c092a0a12ca07a9e3068d7cd00b0a4c52599e60c
JulioLaz <julioalazarte@gmail.com>
2023-09-05 15:12:27 -0300

    primer commit

---
+++ whatsapp.py
@@ -0,0 +1,173 @@
+import tkinter as tk
+from time import sleep
+import os
+from PIL import ImageGrab
+import pyautogui as pt
+
+#Ajuste del Pixel
+pct_x = pt.size()[0]/ImageGrab.grab().size[0]
+pct_y = pt.size()[1]/ImageGrab.grab().size[1]
+
+os.system("start WhatsApp:")
+
+#Enviar Respuesta
+def enviar_respuesta():
+	os.system("start WhatsApp:")
+	posicion = pt.locateCenterOnScreen('imagenes/clip.png', grayscale=False, confidence=.9)
+	if posicion is not None:
+		x = int(posicion[0])*pct_x
+		y = int(posicion[1])*pct_y
+		pt.moveTo(x + 100, y, duration=.5)
+		pt.click()
+		pt.hotkey('ctrl', 'v')
+		pt.typewrite("\n", interval=.01)
+		sleep(1)
+
+#Decidir que responder
+def buscar_respuesta():
+	   # minimizar whatsapp
+	posicion = pt.locateCenterOnScreen('imagenes/minimizar.png', grayscale=False, confidence=.9)
+	if posicion is not None:
+		x = int(posicion[0])*pct_x
+		y = int(posicion[1])*pct_y
+		pt.moveTo(x,y, duration=.05)
+		pt.click()
+		sleep(1)
+		
+		# recargar pagina openai
+	posicion = pt.locateCenterOnScreen('imagenes/chat.png', grayscale=False, confidence=.9)
+	if posicion is not None:
+		x = int(posicion[0])*pct_x
+		y = int(posicion[1])*pct_y
+		pt.moveTo(x,y, duration=.05)
+		pt.click()
+		
+		# pegar texto
+	posicion = pt.locateCenterOnScreen('imagenes/send.png', grayscale=False, confidence=.9)
+	if posicion is not None:
+		x = (int(posicion[0])*pct_x)-100
+		y = int(posicion[1])*pct_y
+		pt.moveTo(x,y, duration=.05)
+		sleep(1)
+		pt.click()
+		pt.hotkey("ctrl", "v")
+		pt.write(". utiliza como max 50 palabras")
+		sleep(3)
+
+		# pegar enviar
+	posicion = pt.locateCenterOnScreen('imagenes/send_verde.png', grayscale=False, confidence=.9)
+	if posicion is not None:
+		x = int(posicion[0])*pct_x
+		y = int(posicion[1])*pct_y
+		pt.moveTo(x,y, duration=.05)
+		pt.click()
+		sleep(6)
+
+		# copy respuesta: 
+	posicion = pt.locateCenterOnScreen('imagenes/copy.png', grayscale=False, confidence=.9)
+	if posicion is not None:
+		x = int(posicion[0])*pct_x
+		y = int(posicion[1])*pct_y
+		pt.moveTo(x,y, duration=.05)
+		sleep(1)
+		pt.click()
+		print("copy")
+		sleep(1)
+	enviar_respuesta()
+	sleep(1)
+		
+#Capturar una foto del mensaje
+def extraer_mensaje():
+
+	posicion = pt.locateCenterOnScreen('imagenes/nuevo.png', grayscale=False, confidence=.9)
+	if posicion is not None:
+		x = int(posicion[0])*pct_x
+		y = int(posicion[1])*pct_y
+		pt.moveTo(x,y, duration=.05)
+		pt.click()
+		sleep(1)
+
+	posicion = pt.locateCenterOnScreen('imagenes/clip.png', grayscale=False, confidence=.9)
+	if posicion is not None:
+		x = int(posicion[0])*pct_x
+		y = int(posicion[1])*pct_y
+		pt.moveTo(x,y, duration=.05)
+		sleep(1)
+		pt.moveTo(x*0.925+30, y*0.92, duration = .05)	
+		pt.click()
+		pt.click()
+		pt.click()
+		sleep(1)
+		pt.hotkey('ctrl', 'c')
+		sleep(1)
+		buscar_respuesta()
+		sleep(1)
+		enviar_respuesta()
+		sleep(1)
+
+#Buscar nuevos mensajes
+def buscar_nuevo_mensaje():
+	posicion = pt.locateCenterOnScreen('imagenes/circulo.png', grayscale=False, confidence=.9)
+	posicion_02 = pt.locateCenterOnScreen('imagenes/circulo_02.png', grayscale=False, confidence=.9)
+	posicion_lupa = pt.locateCenterOnScreen('imagenes/lupa.png', grayscale=False, confidence=.9)
+	if posicion is not None  and posicion_lupa is not None:
+		x = int(posicion[0])*pct_x
+		y = int(posicion[1])*pct_y
+		pt.moveTo(x,y, duration=.05)
+		sleep(1)
+		pt.click()
+		sleep(1)
+		extraer_mensaje()
+	elif posicion_02 is not None  and posicion_lupa is not None:
+		x = int(posicion_02[0])*pct_x
+		y = int(posicion_02[1])*pct_y
+		pt.moveTo(x,y, duration=.05)
+		sleep(1)
+		pt.click()
+		sleep(1)
+		extraer_mensaje()
+	else:
+		sleep(1)
+		posicion = pt.locateCenterOnScreen('imagenes/inicio.png', grayscale=False, confidence=.9)
+		if posicion is not None:
+			x = int(posicion[0])*pct_x
+			y = int(posicion[1])*pct_y
+			pt.moveTo(x-50,y, duration=.05)
+			pt.click()
+		print('No hay nuevos mensajes')
+
+ejecutar_proceso = False
+
+def iniciar_proceso():
+    global ejecutar_proceso
+    ejecutar_proceso = True
+    btn_iniciar.pack_forget()
+    btn_detener.pack(side="top", pady=40)
+    ventana.iconify()
+    ejecutar_busqueda()
+
+def detener_proceso():
+    global ejecutar_proceso
+    ejecutar_proceso = False
+   #  os.system("taskkill /f /im WhatsApp.exe")
+    btn_detener.pack_forget()
+    btn_iniciar.pack(side="top", pady=40)
+
+def ejecutar_busqueda():
+    if ejecutar_proceso:
+        buscar_nuevo_mensaje()
+        ventana.after(5000, ejecutar_busqueda)  # Llama a esta función nuevamente después de 7 segundos
+
+ventana = tk.Tk()
+ventana.title("WhatsApp Bot")
+ventana.geometry("400x200")
+
+frame = tk.Frame(ventana)
+frame.pack(side="bottom", expand=True, padx=50)
+
+btn_iniciar = tk.Button(ventana, text="Iniciar bot de whatsapp", command=iniciar_proceso,bg="#3fd5cd",pady=10,padx=10, font=("Helvetica", 12))
+btn_iniciar.pack(side="top", pady=40)
+
+btn_detener = tk.Button(ventana, text="Detener bot de WhatsApp", command=detener_proceso, pady=10, bg="#a93653", fg="white", font=("Helvetica", 12))
+
+ventana.mainloop()
